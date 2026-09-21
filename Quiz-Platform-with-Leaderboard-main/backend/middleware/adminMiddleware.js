d9ec7825082311f26edebad0a16c1a32b229833c
rajsekhar6666777-png <rajsekhar6666777@gmail.com>
2026-09-21 13:46:32 +0530

    test

--- Quiz-Platform-with-Leaderboard-main/backend/middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-const verifyAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Forbidden. Admin privileges required.' });
-  }
-  next();
-};
-
-module.exports = verifyAdmin;
--- Quiz-Platform-with-Leaderboard-main/backend/routes/leaderboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const leaderboardController = require('../controllers/leaderboardController');
-
-router.get('/', leaderboardController.getLeaderboard);
-router.get('/top', leaderboardController.getTopPlayers);
-
-module.exports = router;
